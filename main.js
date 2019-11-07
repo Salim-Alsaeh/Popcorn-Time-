7be7ca7ca13079e7210ac40b8bb056b1382e689d
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-11-07 16:14:47 +0100

    implemented some MovieList functions

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 
 function MovieList(){
 	var movieList = {};
+
 	movieList.movies = [];
 	movieList.addMovie = addMovie;
 	movieList.removeMovie = removeMovie;
+	
+	return movieList;
 }
 
 function Movie(id, name, genre, description, year) {
@@ -11,11 +14,37 @@ function Movie(id, name, genre, description, year) {
 
 	movie.id = id;
 	movie.name = name;
+	movie.imageUrl = "";
 	movie.genre = genre;
 	movie.description = description;
 	movie.year = year;
 
 	return movie;
+}
+
+var createMovie = function(id, name, genre, description, year){
+	var movie = Movie(id, name, genre, description, year);
+	return movie;
+}
+
+var addMovie = function(movie){
+	this.movies.push(movie);
+}
+
+var removeMovie = function(id){
+	this.movies.forEach(function(element, i){
+		if (element.id === id) {
+			this.movies.splice(i, 1);
+		}
+	});
+}
+
+var getMovie = function(id){
+	this.movies.forEach(function(element, i){
+		if (element.id === id) {
+			return element;
+		}
+	});
 }
 
 
