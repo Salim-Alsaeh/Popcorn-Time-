631126d7837f2242578abbdae9e1bca826aa6fbe
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-11-07 21:43:56 +0100

    implemented getMovie functionality

--- main.js
+++ main.js
@@ -48,10 +48,34 @@ $(document).ready(function () {
 		});
 	};
 
-
 	$('#search-form').on('submit', function (event) {
-		var searchText = $('search-text').val();
+		var searchText = $('#search-text').val();
 		console.log(searchText);
+		getMoviesFromApi(searchText);
 		event.preventDefault();
-	})
+	});
+
+	function getMoviesFromApi(searchInput){
+		axios.get('http://www.omdbapi.com/?s='+ searchInput + '&apikey=2bed335a')
+			.then(function (response) {
+				var movies = response.data.Search;
+				console.log(response);
+				var output = '';
+				$.each(movies, function (i, movie) {
+					output += `
+					<div> 
+						<img src="${movie.Poster}">
+						<h5>${movie.Title}</h5>
+						<a href="#" onclick="selectedMovie('${movie.imdbID}')">Movie Details</a>
+					</div>
+					`
+				});
+				console.log(output);
+				$('#movies').html(output);
+			})
+			.catch(function (error) {
+				console.log(error);
+			})
+	}
+
 });
